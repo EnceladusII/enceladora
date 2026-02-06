aad20e91e8ad1dd02081f7348a4624451af4cbf0
EnceladeII <wittenmeyerguillaume@proton.me>
2026-02-06 17:05:03 +0100

    2026-02-06 Symlink themes

--- scripts/03_hyprlandconf.sh
+++ scripts/03_hyprlandconf.sh
@@ -12,6 +12,9 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
 CONFIG_SRC="$PROJECT_ROOT/.config"
 
+THEMES_DST="$UHOME/.local/share/omadora/themes"
+THEMES_SRC="$PROJECT_ROOT/themes"
+
 timestamp() {
     date +%s
 }
@@ -46,3 +49,34 @@ find "$CONFIG_SRC" -type f | while read -r src; do
 done
 
 echo ".config files merged successfully"
+
+echo "Merging themes"
+
+[[ -d "$THEMES_SRC" ]] || {
+    echo "ERROR: $THEMES_SRC not found"
+    exit 1
+}
+
+for theme_dir in "$THEMES_SRC"/*/; do
+    theme_name="$(basename "$theme_dir")"
+    dst_theme_dir="$THEMES_DST/$theme_name"
+
+    echo "→ Theme: $theme_name"
+
+    find "$theme_dir" -type f | while read -r src; do
+        rel="${src#$theme_dir/}"
+        dst="$dst_theme_dir/$rel"
+
+        mkdir -p "$(dirname "$dst")"
+
+        if [[ -f "$dst" && ! -L "$dst" ]]; then
+            echo "  Backup $dst"
+            backup "$dst"
+        fi
+
+        ln -sfn "$src" "$dst"
+        echo "Symlink $dst → $src"
+    done
+done
+
+echo "Themes merged successfully"
